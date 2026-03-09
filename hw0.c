0af0315c23766d5bc7790c0860104475419addcb
chenredfish <chenredfish940309@gmail.com>
2026-03-09 21:33:00 +0800

    Combine skip list with linked list: fast search with insertion order output

--- hw0.c
+++ hw0.c
@@ -7,7 +7,9 @@
 struct Node {
     char character;
     int count;
-    struct Node *next[MAX_LEVEL];
+    struct Node *next;
+    struct Node *skipNext[MAX_LEVEL];
+    int level;
 };
 
 int randomLevel() {
@@ -22,60 +24,54 @@ struct Node* createNode(char character, int level) {
     struct Node *node = (struct Node*)malloc(sizeof(struct Node));
     node->character = character;
     node->count = 1;
-    for (int i = 0; i <= level; i++) {
-        node->next[i] = NULL;
+    node->next = NULL;
+    node->level = level;
+    for (int i = 0; i < MAX_LEVEL; i++) {
+        node->skipNext[i] = NULL;
     }
     return node;
 }
 
-struct Node* findLetter(struct Node *head, char character) {
+struct Node* findLetterInSkipList(struct Node *head, char character) {
     struct Node *current = head;
     for (int i = MAX_LEVEL - 1; i >= 0; i--) {
-        while (current->next[i] != NULL && current->next[i]->character < character) {
-            current = current->next[i];
+        while (current->skipNext[i] != NULL && current->skipNext[i]->character < character) {
+            current = current->skipNext[i];
         }
     }
-    current = current->next[0];
+    current = current->skipNext[0];
     if (current != NULL && current->character == character) {
         return current;
     }
     return NULL;
 }
 
-struct Node* insertLetter(struct Node *head, char character) {
+void insertLetterInSkipList(struct Node *head, struct Node *newNode) {
     struct Node *update[MAX_LEVEL];
     struct Node *current = head;
     
     for (int i = MAX_LEVEL - 1; i >= 0; i--) {
-        while (current->next[i] != NULL && current->next[i]->character < character) {
-            current = current->next[i];
+        while (current->skipNext[i] != NULL && current->skipNext[i]->character < newNode->character) {
+            current = current->skipNext[i];
         }
         update[i] = current;
     }
     
-    current = current->next[0];
-    
-    if (current != NULL && current->character == character) {
-        current->count++;
-        return current;
+    for (int i = 0; i <= newNode->level; i++) {
+        newNode->skipNext[i] = update[i]->skipNext[i];
+        update[i]->skipNext[i] = newNode;
     }
-    
-    int newLevel = randomLevel();
-    struct Node *newNode = createNode(character, newLevel);
-    
-    for (int i = 0; i <= newLevel; i++) {
-        newNode->next[i] = update[i]->next[i];
-        update[i]->next[i] = newNode;
-    }
-    
-    return newNode;
+}
+
+void appendToList(struct Node *tail, struct Node *newNode) {
+    tail->next = newNode;
 }
 
 void freeList(struct Node *head) {
-    struct Node *current = head->next[0];
+    struct Node *current = head->next;
     while (current != NULL) {
         struct Node *temp = current;
-        current = current->next[0];
+        current = current->next;
         free(temp);
     }
     free(head);
@@ -88,25 +84,39 @@ int main() {
         return 1;
     }
 
-    struct Node *head = createNode('\0', MAX_LEVEL - 1);
+    struct Node *skipListHead = createNode('\0', MAX_LEVEL - 1);
+    struct Node *listHead = createNode('\0', 0);
+    struct Node *listTail = listHead;
     int ch;
 
     while ((ch = fgetc(file)) != EOF) {
         if (ch != ' ' && ch != '\t' && ch != '\n' && ch != '\r') {
             char character = (char)ch;
-            insertLetter(head, character);
+            
+            struct Node *found = findLetterInSkipList(skipListHead, character);
+            if (found != NULL) {
+                found->count++;
+            } else {
+                int level = randomLevel();
+                struct Node *newNode = createNode(character, level);
+                
+                insertLetterInSkipList(skipListHead, newNode);
+                appendToList(listTail, newNode);
+                listTail = newNode;
+            }
         }
     }
 
     fclose(file);
 
-    struct Node *current = head->next[0];
+    struct Node *current = listHead->next;
     while (current != NULL) {
         printf("%c: %d\n", current->character, current->count);
-        current = current->next[0];
+        current = current->next;
     }
 
-    freeList(head);
+    freeList(listHead);
+    freeList(skipListHead);
 
     return 0;
 }
